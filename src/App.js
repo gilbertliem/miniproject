d550b496c512675b1ee6c69ac42c9e9c0857a8b8
gilbert yohanes <yohanesbert97@gmail.com>
2020-10-12 19:51:43 +0700

    fix warning

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import Cover from "./Components/Home/Cover";
 // import HomePage from "./Pages/HomePage";
 
-
-
-
-
-
-
 import Home from "./Pages/Home";
 import User from "./Pages/User";
 import axios from "axios";
@@ -20,68 +14,73 @@ import Footer from "./Components/Footer";
 // ==================== //
 
 export default class App extends Component {
-	
-	state = {
-		detail: [],
-		detailReview: [],
-		searchMovies: [],
-		input: ""
-	}
+  state = {
+    detail: [],
+    detailReview: [],
+    searchMovies: [],
+    input: "",
+  };
 
-	searchHandler = async (keyWords) => {
-		console.log(keyWords);
-		try {
-		  this.setState({input: keyWords});
-		  const submit = await axios({
-			method: "get",
-			url: "https://damp-dawn-67180.herokuapp.com/movie/search?search=" + keyWords,
-		  });
-		  console.log(submit);
-		  this.setState({ searchMovies: submit.data });
-		} catch (error) {
-		  console.log(error);
-		}
-	  };
+  searchHandler = async (keyWords) => {
+    // console.log(keyWords);
+    try {
+      this.setState({ input: keyWords });
+      const submit = await axios({
+        method: "get",
+        url:
+          "https://damp-dawn-67180.herokuapp.com/movie/search?search=" +
+          keyWords,
+      });
+      //   console.log(submit);
+      this.setState({ searchMovies: submit.data });
+    } catch (error) {
+      //   console.log(error);
+    }
+  };
 
-	
-	detailsHandler = (id) =>{
-		axios.get('https://damp-dawn-67180.herokuapp.com/movie/detail/' + id)
-			.then(response => {
-				this.setState({
-					detail: response.data.detail,
-					detailReview: response.data.reviewmovie
-				},
-							 console.log("detail review" + this.state.detailReview))
-				console.log(response)
-				console.log('CLICKCKKK')
-			})
-	}
-	
+  detailsHandler = (id) => {
+    axios
+      .get("https://damp-dawn-67180.herokuapp.com/movie/detail/" + id)
+      .then((response) => {
+        this.setState(
+          {
+            detail: response.data.detail,
+            detailReview: response.data.reviewmovie,
+          },
+          console.log("detail review" + this.state.detailReview)
+        );
+        console.log(response);
+        console.log("CLICKCKKK");
+      });
+  };
+
   render() {
     return (
       // DETAIL PAGE
       <Router>
         <Switch>
-		  <Route exact path="/">
-		  	<Home 
-				searchHandler={this.searchHandler}
-				detailsHandler={this.detailsHandler}
-				/>
-		  </Route>
-			<Route path="/search/:search">
-				<Search 
-					input={this.state.input}
-					detailsHandler={this.detailsHandler}
-					searchHandler={this.searchHandler}
-					search={this.state.searchMovies}/>
-			</Route>
+          <Route exact path="/">
+            <Home
+              searchHandler={this.searchHandler}
+              detailsHandler={this.detailsHandler}
+            />
+          </Route>
+          <Route path="/search/:search">
+            <Search
+              input={this.state.input}
+              detailsHandler={this.detailsHandler}
+              searchHandler={this.searchHandler}
+              search={this.state.searchMovies}
+            />
+          </Route>
           <Route path="/detail/:id">
-			<Detail 
-				detailReview={this.state.detailReview}
-				detail={this.state.detail}/>
-		  </Route>
-		  <Route path="/user" component={User} />
-		  <Route path="/category" component={Home}/>
+            <Detail
+              detailReview={this.state.detailReview}
+              detail={this.state.detail}
+            />
+          </Route>
+          <Route path="/user" component={User} />
+          <Route path="/category" component={Home} />
         </Switch>
         <Footer />
       </Router>
